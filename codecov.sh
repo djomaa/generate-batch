9adac87f81713588ffeb24a1e2895d6d1b687cf3
djooma <demid.mosko@gmail.com>
2021-04-14 20:08:55 +0300

    update

--- codecov.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-source .env
-
-if [ -z "$CODECOV_TOKEN" ]; then echo "CODECOV_TOKEN not provided"; exit 1; fi
-
-npx nyc report --reporter=json
-bash <(curl -s https://codecov.io/bash) -f ./coverage/coverage-final.json -t $CODECOV_TOKEN
